6b395c6e96d4413fcb9e4012b66059abf1715315
B. Hrnja <b@hrnja.com>
2024-04-21 15:18:01 -0400

    Fixed previous Syntax already declared error Changing the variables of computerChoice and playerChoice to the arguments of the playRound function of playerSelection and computerSelection seems to have fixed the syntax error hindering further progress.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const computerChoice = ["Rock", "Paper", "Scissors"];
+const computerSelection = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    return computerChoice[(Math.random() * computerChoice.length) | 0];
+    return computerSelection[(Math.random() * computerSelection.length) | 0];
 }
 
 console.log("The computer chose " + getComputerChoice() + "!");
 
-const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = playerChoice;
-    let computerSelection = computerChoice;
+    
 }
