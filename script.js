1f073fd8f033cd71bd3d9ab319f20c6ca0d47c52
B. Hrnja <b@hrnja.com>
2024-04-21 16:19:20 -0400

    Further work on playRound function achieved
    Starting to get closer to printing and comparing the results
    of a "Game Round". Current trouble is that a statement like
    ```
    if (playerSelection === "rock" && computerSelection === "Rock") {
            return "It's a tie! Rock can't beat Rock.";
        }
    ```
    returns an "undefined" result in the console. Will be continuing work
    on this program.

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ console.log("The computer chose " + getComputerChoice() + "!");
 
 const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
+console.log("The player chose " + playerSelection + "!");
+
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === "rock" && computerSelection === "Rock") {
+        return "It's a tie! Rock can't beat Rock.";
+    }
+        else if (playerSelection === "paper" && computerSelection === "Paper") {
+            return "It's a tie! Paper can't beat Paper.";
+        }
+
+        else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+            return "It's a tie! Scissors can't beat Scissors.";
+        }
+
+        else if (playerSelection === "rock" && computerSelection === "Scissors") {
+            return "It's a tie! Scissors can't beat Scissors.";
+        }
 }
+
+console.log(playRound(playerSelection, getComputerChoice))
