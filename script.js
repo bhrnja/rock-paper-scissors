89c16e1a85abb52eec5ee904a2afe3aff553dca3
B. Hrnja <b@hrnja.com>
2024-04-21 23:51:40 -0400

    Solved the problem of undefined getComputerChoice
    After some rearranging of variables and console logs,
    I was able to solve the problem of an undefined computer
    selection by changing the top computerSelection constant
    into its own storage variable. This also allowed me to
    declare constants of computerSelection and playerSelection
    after the playRound function has been written, and before
    the playRound function is called.

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-const computerSelection = ["Rock", "Paper", "Scissors"];
+// The below function allows the "computer" to choose an array item at random.
+const computerChoice = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    return computerSelection[(Math.random() * computerSelection.length) | 0];
+    return computerChoice[(Math.random() * computerChoice.length) | 0];
 }
 
-console.log("The computer chose " + getComputerChoice() + "!");
-
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-
-console.log("The player chose " + playerSelection + "!");
-
+// The below function is the way this program compares the player and computer selection together. 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "Rock") {
         return "It's a tie! Rock can't beat Rock.";
     }
+        else if (playerSelection === "rock" && computerSelection === "Scissors") {
+            return "You win! Rock beats Scissors.";
+        }
+        else if (playerSelection === "rock" && computerSelection === "Paper") {
+            return "You lose! Paper beats Rock.";
+        }
         else if (playerSelection === "paper" && computerSelection === "Paper") {
             return "It's a tie! Paper can't beat Paper.";
         }
-
+        else if (playerSelection === "paper" && computerSelection === "Rock") {
+            return "You win! Paper beats Rock.";
+        }
+        else if (playerSelection === "paper" && computerSelection === "Scissors") {
+            return "You lose! Scissors beats Paper.";
+        }
         else if (playerSelection === "scissors" && computerSelection === "Scissors") {
             return "It's a tie! Scissors can't beat Scissors.";
         }
-
-        else if (playerSelection === "rock" && computerSelection === "Scissors") {
-            return "It's a tie! Scissors can't beat Scissors.";
+        else if (playerSelection === "scissors" && computerSelection === "Rock") {
+            return "You lose! Rock beats Scissors.";
+        }
+        else if (playerSelection === "scissors" && computerSelection === "Paper") {
+            return "You win! Scissors beats Paper.";
+        }
+        else {
+            return "That's not a choice!"
         }
 }
 
-console.log(playRound(playerSelection, getComputerChoice));
+// Variables that set and print the answer to the console.
+const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+console.log("The computer chose " + computerSelection + "!");
+console.log("The player chose " + playerSelection + "!");
