bf4910e13a692230820dce7694c914e5328c5bb1
B. Hrnja <b@hrnja.com>
2024-04-23 22:49:19 -0400

    Removed script_test.js and moved to script.js
    Also fixed a typo on line 64 of script.js that
    caused the program to return a losing starting
    even if the player won the game.

--- script.js
+++ script.js
@@ -5,44 +5,76 @@ function getComputerChoice() {
     return computerChoice[(Math.random() * computerChoice.length) | 0];
 }
 
-// The below function is the way this program compares the player and computer selection together. 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "Rock") {
-        return "It's a tie! Rock can't beat Rock.";
+// The below function is what plays the game of rock, paper, scissors.
+// The empty string variables of result and gameWinner allows the program
+// to keep track of the progress of the game round by round and relay that
+// back to the console.
+function playGame(i) {
+    let result = "";
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameWinner = "";
+    // This loop establishes that the game will run for six rounds.
+    for (let i = 1; i < 6; i++) {
+        const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
+
+        if (playerSelection === "rock" && computerSelection === "Rock") {
+            result += `Round ${i}: ` + "It's a tie! Rock can't beat Rock.\n";
+        }
+            // Alongside appending to the "result" variable. Here we're adding to
+            // another tracker, playerScore and computerScore.
+            else if (playerSelection === "rock" && computerSelection === "Scissors") {
+                result += `Round ${i}: ` + "You win! Rock beats Scissors.\n";
+                playerScore += 1;
+            }
+            else if (playerSelection === "rock" && computerSelection === "Paper") {
+                result += `Round ${i}: ` + "You lose! Paper beats Rock.\n";
+                computerScore += 1;
+            }
+            else if (playerSelection === "paper" && computerSelection === "Paper") {
+                result += `Round ${i}: ` + "It's a tie! Paper can't beat Paper.\n";
+            }
+            else if (playerSelection === "paper" && computerSelection === "Rock") {
+                result += `Round ${i}: ` + "You win! Paper beats Rock.\n";
+                playerScore += 1;
+            }
+            else if (playerSelection === "paper" && computerSelection === "Scissors") {
+                result += `Round ${i}: ` + "You lose! Scissors beats Paper.\n";
+                computerScore += 1;
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+                result += `Round ${i}: ` + "It's a tie! Scissors can't beat Scissors.\n";
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Rock") {
+                result += `Round ${i}: ` + "You lose! Rock beats Scissors.\n";
+                computerScore += 1;
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Paper") {
+                result += `Round ${i}: ` + "You win! Scissors beats Paper.\n";
+                playerScore += 1;
+            }
+            else {
+                result += `Round ${i}: ` + "That's not a choice!\n"
+            }
     }
-        else if (playerSelection === "rock" && computerSelection === "Scissors") {
-            return "You win! Rock beats Scissors.";
-        }
-        else if (playerSelection === "rock" && computerSelection === "Paper") {
-            return "You lose! Paper beats Rock.";
-        }
-        else if (playerSelection === "paper" && computerSelection === "Paper") {
-            return "It's a tie! Paper can't beat Paper.";
-        }
-        else if (playerSelection === "paper" && computerSelection === "Rock") {
-            return "You win! Paper beats Rock.";
-        }
-        else if (playerSelection === "paper" && computerSelection === "Scissors") {
-            return "You lose! Scissors beats Paper.";
-        }
-        else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-            return "It's a tie! Scissors can't beat Scissors.";
-        }
-        else if (playerSelection === "scissors" && computerSelection === "Rock") {
-            return "You lose! Rock beats Scissors.";
-        }
-        else if (playerSelection === "scissors" && computerSelection === "Paper") {
-            return "You win! Scissors beats Paper.";
-        }
+
+    // Comparing playerScore and computerScore to decide and append the winner
+    // of the game to gameWinner.
+    if (playerScore > computerScore) {
+        gameWinner += "You won the game!"
+    }
         else {
-            return "That's not a choice!"
+            gameWinner += "You lost the game."
         }
+    
+    // All log and return statements needed for the player to know the progress of the game.
+    console.log(`You won ${playerScore} round(s) out of 5!`)
+    console.log(`The computer won ${computerScore} round(s) out of 5!`)
+    console.log(gameWinner)
+    return result;
+
 }
 
-// Variables that set and print the answer to the console.
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
-console.log("The computer chose " + computerSelection + "!");
-console.log("The player chose " + playerSelection + "!");
+// Function call to  play the game in the console.
+console.log(playGame());
