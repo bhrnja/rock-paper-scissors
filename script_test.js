84a01302fc1c86a0a05a2540b312a1b5d3b7c0ff
B. Hrnja <b@hrnja.com>
2024-04-23 21:31:52 -0400

    Further added to the new iterating rps program
    I've successfully made the game play five round of rock, paper, scissors
    by including my code from the old script into the new iterating playGame function.
    To make it work, I replaced all "return" values with the appending "result" value
    upon each iteration of the program. I'm getting close to the end for now!
    I now need to work on a way for the game to pause or wait before prompting the user
    for another round.

--- script_test.js
+++ script_test.js
@@ -1,3 +1,4 @@
+// The below function allows the "computer" to choose an array item at random.
 const computerChoice = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
@@ -7,8 +8,42 @@ function getComputerChoice() {
 function playGame(i) {
     let result = "";
     for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
 
-        result += "test\n";
+        if (playerSelection === "rock" && computerSelection === "Rock") {
+            result += "It's a tie! Rock can't beat Rock.";
+        }
+            else if (playerSelection === "rock" && computerSelection === "Scissors") {
+                result += "You win! Rock beats Scissors.";
+            }
+            else if (playerSelection === "rock" && computerSelection === "Paper") {
+                result += "You lose! Paper beats Rock.";
+            }
+            else if (playerSelection === "paper" && computerSelection === "Paper") {
+                result += "It's a tie! Paper can't beat Paper.";
+            }
+            else if (playerSelection === "paper" && computerSelection === "Rock") {
+                result += "You win! Paper beats Rock.";
+            }
+            else if (playerSelection === "paper" && computerSelection === "Scissors") {
+                result += "You lose! Scissors beats Paper.";
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+                result += "It's a tie! Scissors can't beat Scissors.";
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Rock") {
+                result += "You lose! Rock beats Scissors.";
+            }
+            else if (playerSelection === "scissors" && computerSelection === "Paper") {
+                result += "You win! Scissors beats Paper.";
+            }
+            else {
+                result += "That's not a choice!"
+            }
+
+
+        // We're getting somewhere!
 
     }
 
